22a38fbcae5eb9cd6d36acbff55eeb7d350f1cab
VladVes <veselov.workflow@gmail.com>
2019-08-15 17:14:45 +0700

    update

---
+++ LitElemComp/src/MyLitComponent.js
@@ -0,0 +1,9 @@
+import { LitElement, html } from 'lit-element';
+
+export default class extends LitElement {
+  
+  render() {
+    return html`<p>Lit-element Component here</p>`;
+
+  }
+}
---
+++ LitElemComp/src/index.js
@@ -0,0 +1,3 @@
+import MyLitComponent from './MyLitComponent.js';
+
+window.customElements.define('my-litcomp', MyLitComponent);
